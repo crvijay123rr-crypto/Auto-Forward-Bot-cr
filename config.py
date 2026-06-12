ab8a9c187af0c985914668fbefef7837edbd6095
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-06-13 05:20:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
-from os import environ 
+from os import environ
 
 class Config:
-    API_ID = environ.get("API_ID", "577678")
-    API_HASH = environ.get("API_HASH", "d2c6e01uuiuiouioiuiou0fc6d7a1be")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "70955...") 
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
+    # Environment variables set karein, default values hata dena chahiye
+    API_ID = int(environ.get("API_ID", 24894984))
+    API_HASH = environ.get("API_HASH", "4956e23833905463efb588eb806f9804")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "")
+    BOT_SESSION = environ.get("BOT_SESSION", "bot")
+    DATABASE_URI = environ.get("DATABASE", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6964148334').split()]
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '8183010692').split()]
 
-class temp(object): 
+class temp(object):
     lock = {}
     CANCEL = {}
     forwardings = 0
